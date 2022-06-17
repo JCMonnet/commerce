7b4a38b5b1de94a4351aaa7a6ede73c6aaa2f62a
JCMonnet <jcmonnet0408@gmail.com>
2022-06-17 11:51:40 +0200

    création classe et objets produits

---
+++ app.js
@@ -0,0 +1,13 @@
+    let products=[]
+    let cd1= new Product ('michaël Gregorio','43,00 Eu','false','non')
+    let cd2= new Product ('daniel Guichard','43,00 Eu','false','20%')
+    let pc1= new Product ('PC Portable Gaming MSI GL75 Leopard 10SFK-457FR 17,3 Intel Core I7 16 Go RAM 256 Go SSD + 1 To SATA Noir','1999.99 Eu','true','20%')
+    let pc2= new Product ('PC Portable Gaming AsusTUF505DVHN232T 15.6"AMD Ryzen 7 16Go RAM 512 GoSSD Noir','1499,99 Eu','true','33%')
+    let pc3= new Product ('PC Portable Gaming AcerPredator Triton700 PT715-51-76D4 15.6"Gaming IntelCore i7 32 GoRAM 256 Go SSD+ 256 Go SATANoir','3499,99 Eu','false','non')
+    products.push(cd1,cd2,pc1,pc2,pc3)
+
+    
+    
+    
+    
+
---
+++ classe.js
@@ -0,0 +1,11 @@
+class Product {
+    constructor(name, price, promotion, discount) {
+        this.name = name
+        this.price = price
+        this.promotion = promotion
+        this.discount = discount
+    }
+    affichePromo() {
+    }
+}
+
